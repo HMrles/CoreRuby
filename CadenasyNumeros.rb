d64bf5c895b24a8fec3349150af6072460678c2d
HMrles <hmorales@gmx.com>
2014-12-06 19:59:25 -0600

    Manejo de Cadenas y numeros

---
+++ CadenasyNumeros.rb
@@ -0,0 +1,17 @@
+puts "Introduccion a  cadenas"
+
+puts "Ejemplo de iteracion sin bucle"
+puts "Ruby\n"*3
+
+puts "\nSuma de dos Enteros"
+puts  16 +16
+
+puts "\nConcatenacion  "
+puts "16"+"16"
+
+puts "\nCadena Normal"
+puts "16 + 16"
+
+
+puts 'Ruby con "comillas" dobles'
+gets()
