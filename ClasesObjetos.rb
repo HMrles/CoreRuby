5dbdc46b9ef5db527ecf04118aeeebd09c97acca
HMrles <hmorales@gmx.com>
2014-12-06 20:10:24 -0600

    Definicion de Clase

---
+++ ClasesObjetos.rb
@@ -0,0 +1,14 @@
+#Definicion de la Clase
+class Principal
+	def initialize() #Constructor normal 
+	end
+
+	def mensaje()
+		puts "Invocar el metodo mensaje()"
+	end
+end
+
+#Instanciamos la clase Principal
+clase = Principal.new()
+clase.mensaje()
+gets()
